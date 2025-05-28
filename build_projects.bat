1490b494849b4643adffd9609a028da03e974438
Harrison Mclean <h.mclean@westnet.com.au>
2025-05-29 04:09:13 +0800

    Update build_projects.bat
    Copy the EXE file for each macro out to a common "macro_release" folder, will be one folder up from "generated_commands" folder, usually in same dir as "stratagema.exe"

--- build_projects.bat
+++ build_projects.bat
@@ -1,18 +1,34 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Set the root folder where the Rust projects are located
-set ROOT_DIR="%cd%"
+:: Correctly set the root folder (NO quotes inside the SET)
+set ROOT_DIR=%cd%
+set RELEASE_DIR=%ROOT_DIR%\..\macro_release
+
+echo ROOT_DIR is %ROOT_DIR%
+echo RELEASE_DIR is %RELEASE_DIR%
+
+:: Create the release folder if it doesn't exist
+if not exist "%RELEASE_DIR%" (
+    mkdir "%RELEASE_DIR%"
+)
 
 :: Loop through all directories in ROOT_DIR
-for /d %%D in (%ROOT_DIR%\*) do (
+for /d %%D in ("%ROOT_DIR%\*") do (
     if exist "%%D\Cargo.toml" (
         echo Building project in %%D...
-        pushd %%D
+        pushd "%%D"
         cargo build --release
+
+        :: find .exe file under target\release
+        for %%F in ("target\release\*.exe") do (
+            echo Copying %%~nxF to "%RELEASE_DIR%"...
+            copy "%%F" "%RELEASE_DIR%\%%~nxF" >nul
+        )
+
         popd
     )
 )
 
-echo All projects have been built.
+echo All projects have been built and executables copied to the release folder.
 pause
